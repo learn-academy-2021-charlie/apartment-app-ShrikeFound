7559c4cbbda8db62acb1bc955786b5d328983154
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-31 10:00:33 -0700

    adds login stuff

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,16 +3,33 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import About from './pages/About'
 import Home from './pages/Home'
-
+import JobsIndex from './pages/Jobs/JobsIndex'
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      jobs: []
+    }
+  }
+
+
+
+
   render() {
+    const {logged_in,new_user_route,sign_in_route,sign_out_route,current_user} = this.props
     return (
       <Router>
         <div className=" w-10/12 max-w-6xl mx-auto font-mono ">
-          <Header/>
+          <Header 
+            logged_in={logged_in} 
+            new_user_route={new_user_route} 
+            sign_in_route={sign_in_route}
+            sign_out_route={sign_out_route}
+          />
           <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
+            <Route path="/jobs" component={JobsIndex}/>
           </Switch>
         </div>
       </Router>
--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -3,13 +3,18 @@ import {Link} from 'react-router-dom'
 
 export class Header extends Component {
   render() {
+    const {logged_in,sign_out_route,sign_in_route} = this.props
+    console.log("sign in route: ",sign_in_route)
+    console.log("sign out route: ",sign_out_route)
+    console.log("loggedIn: ",logged_in)
     return (
       <header className=" mt-12 mb-20 flex justify-between">
         
         <Link to="/">RANGER | Job Hunt Tracker</Link>
         <nav >
-          <Link to="/" className="p-2 hover:bg-yellow-300 focus:bg-yellow-300">Home</Link>
-          <Link to="/about" className="ml-4 p-2 hover:bg-yellow-300 focus:bg-yellow-300">About</Link>
+          <Link to="/jobs" className="nav-link">Jobs</Link>
+          <Link to="/about" className="ml-4 nav-link">About</Link>
+          {logged_in ? <a className="ml-4 nav-link" href={sign_out_route}>Log Out</a> : <a className="ml-4 nav-link" href={sign_in_route}>Log In</a>}
         </nav>
 
       </header>
