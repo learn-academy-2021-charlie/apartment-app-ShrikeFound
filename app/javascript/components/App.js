000fdf14889413ca0ac8b701b92b35b0d83c3742
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-30 21:33:10 -0700

    added react routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import About from './pages/About'
+import Home from './pages/Home'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Router>
+        <br/>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+        </Switch>
+      </Router>
     )
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :jobs
   devise_for :users
+  get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   root 'home#index'  
 end
