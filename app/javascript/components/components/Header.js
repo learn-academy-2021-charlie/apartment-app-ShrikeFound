cd319db09baf66e6a4927cee9853dfc54bcc3341
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-30 23:26:17 -0700

    figured out how to apply multiple styles to one class

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom'
 export class Header extends Component {
   render() {
     return (
-      <header className=" my-4 flex justify-between">
+      <header className=" mt-12 mb-20 flex justify-between">
         
         <Link to="/">RANGER | Job Hunt Tracker</Link>
         <nav >
-          <Link to="/" className="p-2 hover:bg-green-300 focus:bg-green-300">Home</Link>
-          <Link to="/about" className="ml-4 p-2 hover:bg-green-300 focus:bg-green-300">About</Link>
+          <Link to="/" className="p-2 hover:bg-yellow-300 focus:bg-yellow-300">Home</Link>
+          <Link to="/about" className="ml-4 p-2 hover:bg-yellow-300 focus:bg-yellow-300">About</Link>
         </nav>
 
       </header>
--- app/javascript/components/pages/About.js
+++ app/javascript/components/pages/About.js
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h1>About Us</h1>
+       <h1 className="page-header">About Us</h1>
       </div>
     )
   }
