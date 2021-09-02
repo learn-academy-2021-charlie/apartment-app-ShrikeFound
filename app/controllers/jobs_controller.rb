1a7e1c4a35f55a9bc4c55b3e3d40cd0396cde689
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-09-02 15:11:19 -0700

    added updat endpoint, testing

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -24,12 +24,18 @@ class JobsController < ApplicationController
     if job.save 
       render json: job, include: ['status']
     else
-      render animal.errors.full_messages
+      render job.errors.full_messages
     end
 
   end
 
   def update
+    job = Job.find(params[:id])
+    if job.update(job_params)
+      render json: job, include: ['status']
+    else
+      render job.errors.full_messages
+    end
   end
 
   def destroy
@@ -39,7 +45,7 @@ class JobsController < ApplicationController
   private
   
   def job_params 
-    params.require(:job).permit(:title,:city,:state,:url,:description,:confidence_level,:salary,:is_remote,:notes,:is_private)
+    params.require(:job).permit(:title,:city,:state,:url,:description,:confidence_level,:salary,:is_remote,:notes,:is_private,:company)
   end
 
 
--- spec/requests/jobs_spec.rb
+++ spec/requests/jobs_spec.rb
@@ -1,51 +1,93 @@
 require 'rails_helper'
 
-RSpec.describe "Jobs", type: :request do
-  
-  describe "GET /index" do
-
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+RSpec.describe 'Jobs', type: :request do
+  # describe 'GET /index' do
+  #   pending "add some examples (or delete) #{__FILE__}"
+  # end
 
   describe 'POST /jobs' do
-    
     it 'creates a job' do
-
       user_attrs = {
-        email: "job.seeker@email.com",
-        password: "learn_jobs_123",
-        password_confirmation: "learn_jobs_123"
+        email: 'job.seeker@email.com',
+        password: 'learn_jobs_123',
+        password_confirmation: 'learn_jobs_123',
       }
-      
-      status = Status.create(name:"lead",priority:1)
+
+      status = Status.create(name: 'lead', priority: 1)
       user = User.create(user_attrs)
       sign_in user
 
       job_params = {
-        job:{
-          company: "Terrier Tech",
-          title: "Software Engineer",
-          city: "San Diego",
-          state: "California",
-          url: "www.google.com",
-          description: "this is where the job description would go. for now it's just this.",
+        job: {
+          company: 'Terrier Tech',
+          title: 'Software Engineer',
+          city: 'San Diego',
+          state: 'California',
+          url: 'www.google.com',
+          description:
+            "this is where the job description would go. for now it's just this.",
           confidence_level: 5,
           status_id: status.id,
-          salary: 6000000,
+          salary: 6_000_000,
           is_remote: false,
           is_private: true,
-          notes: "these are where I'd add my own personal notes about the job"
-        }
+          notes: "these are where I'd add my own personal notes about the job",
+        },
       }
 
-
-
       post '/jobs', params: job_params
+      job = Job.first
 
-      # expect(response).to have_http_status(200)
-
+      expect(response).to have_http_status(200)
+      expect(job.company).to eq("Terrier Tech")
     end
-      
-  end
 
+    describe 'PATCH /jobs/:id' do
+      it 'updates a job' do
+        user_attrs = {
+          email: 'job.seeker@email.com',
+          password: 'learn_jobs_123',
+          password_confirmation: 'learn_jobs_123',
+        }
+
+        status = Status.create(name: 'lead', priority: 1)
+        user = User.create(user_attrs)
+        sign_in user
+
+        job_params = {
+          job: {
+            company: 'Terrier Tech',
+            title: 'Software Engineer',
+            city: 'San Diego',
+            state: 'California',
+            url: 'www.google.com',
+            description:
+              "this is where the job description would go. for now it's just this.",
+            confidence_level: 5,
+            status_id: status.id,
+            salary: 6_000_000,
+            is_remote: false,
+            is_private: true,
+            notes:
+              "these are where I'd add my own personal notes about the job",
+          },
+        }
+
+        job = user.jobs.create(job_params[:job])
+
+        job_edit_params = {
+          job: {
+            title: "CEO"
+          }
+        }
+
+        patch "/jobs/#{job.id}", params: job_edit_params
+
+        edited_job = Job.first
+
+        expect(response).to have_http_status(200)
+        expect(edited_job.title).to eq("CEO")
+      end
+    end
+  end
 end
