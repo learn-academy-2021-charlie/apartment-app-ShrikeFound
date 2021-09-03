0117f62a73f3a94bacc43d97991eacaac37f31b1
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-09-02 19:13:52 -0700

    styles button. yup.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -5,57 +5,84 @@ import ProtectedRoute from './components/ProtectedRoute'
 import About from './pages/About'
 import Home from './pages/Home'
 import JobsIndex from './pages/Jobs/JobsIndex'
-import JobsShow from './pages/Jobs/JobsShow'
+import JobsNew from "./pages/Jobs/JobsNew"
+import JobsShow from "./pages/Jobs/JobsShow"
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      jobs: []
+      jobs: [],
     }
   }
 
-
-  getJobs = async () =>{
+  getJobs = async () => {
     const loggedIn = this.props.logged_in
-    const url = loggedIn ? this.props.user_jobs_route : this.props.sample_jobs_route
+    const url = loggedIn
+      ? this.props.user_jobs_route
+      : this.props.sample_jobs_route
     const response = await fetch(url)
     const result = await response.json()
-    this.setState({jobs: result})
+    this.setState({ jobs: result })
   }
 
-
-
-
-  componentDidMount(){
-    this.getJobs();
-
+  componentDidMount() {
+    this.getJobs()
   }
 
   render() {
-    const {logged_in,new_user_route,sign_in_route,sign_out_route,current_user} = this.props
+    const {
+      logged_in,
+      new_user_route,
+      sign_in_route,
+      sign_out_route,
+      current_user,
+    } = this.props
     return (
       <Router>
         <div className=" w-10/12 max-w-6xl mx-auto font-mono ">
-          <Header 
-            logged_in={logged_in} 
-            new_user_route={new_user_route} 
+          <Header
+            logged_in={logged_in}
+            new_user_route={new_user_route}
             sign_in_route={sign_in_route}
             sign_out_route={sign_out_route}
           />
           <Switch>
-            <Route exact path="/" render = {(props) => <Home jobs={this.state.jobs}/>}/>
-            <Route path="/about" component={About}/>
-            <ProtectedRoute path="/jobsindex"sign_in_route={sign_in_route} logged_in={logged_in}>
-              <Route render = {(props) =>{
-                return <JobsIndex jobs={this.state.jobs} />
-              }}/>
+            <Route
+              exact
+              path="/"
+              render={(props) => <Home jobs={this.state.jobs} />}
+            />
+            <Route path="/about" component={About} />
+            <ProtectedRoute
+              path="/jobsindex"
+              sign_in_route={sign_in_route}
+              logged_in={logged_in}
+            >
+              <Route
+                render={(props) => {
+                  return <JobsIndex jobs={this.state.jobs} />
+                }}
+              />
             </ProtectedRoute>
-            <Route path="/jobsshow/:id" render = {(props) =>{
-              const id = props.match.params.id 
-              const job = this.state.jobs.find(job => job.id === +id)
-              return <JobsShow job={job}/>
-
-            }}/>
+            <ProtectedRoute
+              path="/jobsnew"
+              sign_in_route={sign_in_route}
+              logged_in={logged_in}
+            >
+              <Route
+                render={(props) => {
+                  return <JobsNew />
+                }}
+              />
+            </ProtectedRoute>
+            <Route
+              path="/jobsshow/:id"
+              render={(props) => {
+                const id = props.match.params.id
+                const job = this.state.jobs.find((job) => job.id === +id)
+                return <JobsShow job={job} />
+              }}
+            />
           </Switch>
         </div>
       </Router>
